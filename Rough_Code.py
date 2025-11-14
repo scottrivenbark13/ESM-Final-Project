ed5dfa06f08452b694c9f1bde88707781798baaf
justin-c604 <Justinlcarter44@gmail.com>
2025-11-13 23:28:31 -0500

    Update Rough_Code.py

---
+++ Rough_Code.py
@@ -0,0 +1,15 @@
+# rough idea of the G equation
+# correct variable values later
+a = .81e-3;
+b = .3e-6;
+E0 = 1;
+k = 1;
+dQ = 1;
+E = E0+(k*dQ);
+h = []
+h_prime = []
+G = 0;
+if h+h'-E <= 1500 
+    G = a*(h+h'-E)-b(h+h'-E)^2
+else 
+    G = .56;
