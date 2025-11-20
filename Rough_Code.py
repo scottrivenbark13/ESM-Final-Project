3c5ffd825844baaf8ec1f7213accabb6b7d43379
Scott Rivenbark <wrivenbark3@gatech.edu>
2025-11-19 20:05:00 -0500

    Fixed ice density function

--- Rough_Code.py
+++ Rough_Code.py
@@ -51,7 +51,7 @@ def initialize(numberPoints, dt):
     values['nu'] = 100
     values['v'] = 100000 * 1000 # km2 / yr, converted to meters 
 
-    values['dx'] = (74-30) / numberPoints
+    values['dx'] = (74-30) / (numberPoints - 1)
     values['dx_m'] = values['dx'] * 111000
 
 
@@ -66,7 +66,7 @@ def initialize(numberPoints, dt):
 v = initialize(80, 50) # Test values from paper, 80 points and 50 year timesteps
 
 def bedrockM (size): #Creating the matricies that will be used to solve equation 3, DOES NOT NEED TO BE CALLED MORE THAN ONCE
-    arr_size = size + 1
+    arr_size = size
     cD = v['v'] * v['dt'] / v['dx_m'] ** 2
     ML_Data = np.array([[-0.5*cD]*arr_size,[1+cD]*arr_size,[-0.5*cD]*arr_size])
     MLdiag = np.array([-1,0,1])
@@ -77,16 +77,17 @@ def bedrockM (size): #Creating the matricies that will be used to solve equation
     MR = sp.sparse.spdiags(MR_Data, MRdiag, arr_size, arr_size).toarray()
 
     # Applying Dirichlet conditions to arrays
-    ML[0, 0] = 1
-    MR[0, 0] = 1
-    ML[arr_size-1, arr_size-1] = 1
-    MR[arr_size-1, arr_size-1] = 1
-
     # not sure why the entire row needs to be zero, had to look it up to get this function working, LOOK INTO THIS
-    ML[arr_size-1, :] = 0 
+    ML[0, :] = 0
+    ML[0, 0] = 1
+    MR[0, :] = 0
+    MR[0, 0] = 1
+
+    ML[arr_size-1, :] = 0
     ML[arr_size-1, arr_size-1] = 1
     MR[arr_size-1, :] = 0
     MR[arr_size-1, arr_size-1] = 1
+
 
     return ML, MR
 
@@ -108,7 +109,8 @@ def massBal(h, h_prime, timestep):
     
     return G
 
-
+'''
+first iteration of ice density function
 # Function for Ice density matricies, this is not a one and done call. 
 def iceDensity(size):
     arr_size = size + 1
@@ -124,13 +126,69 @@ def iceDensity(size):
     left = diags([a, b, c], [-1, 0, 1], shape=(space_step, space_step)) # create the left hand matrix
     right = np.zeros(arr_size,1) #create the rhs matrix 
     return left,right
+'''
+
+def iceDensity(h, h_prime):
+    noZeroMath = 1e-10 # prevents h and the gradient from being zero, cause that would cause a lot of problems 
+    dx = v['dx_m']
+    n = len(h)
+    elevation = h + h_prime
+    gradient = np.zeros(len(elevation))
+    for i in range(1, n - 1):
+        gradient[i] = (elevation[i + 1] - elevation[i-1]) / (2 * dx)
+    
+    # boundary conditions for gradient array
+    gradient[0] = (elevation[1] - elevation[0]) / dx
+    gradient[-1] = (elevation[-1] - elevation[-2]) / dx
+    
+    diffuse = v['A'] * (h + noZeroMath) ** v['alpha'] * (np.abs(gradient) + noZeroMath) ** v['beta']
+
+    # Different way of creating the matricies than the bedrock due to the fact that we have to change them each timestep
+    # we have to recalculate our "cD" each time
+    lower = np.zeros(n - 1)
+    mid = np.ones(n)
+    upper = np.zeros(n - 1)
+    
+    for i in range(1, n-1):
+        D_left = 0.5 * (diffuse[i-1] + diffuse[i])
+        D_right = 0.5 * (diffuse[i] + diffuse[i+1])
+        
+        r_left = 0.5 * v['dt'] * D_left / dx ** 2
+        r_right = 0.5 * v['dt'] * D_right / dx ** 2
+        
+        lower[i-1] = -r_left
+        mid[i] = 1 + r_left + r_right
+        upper[i] = -r_right
+
+    # creating ML
+    mid[0] = 1
+    upper[0] = 0
+    ML_Data = [lower, mid, upper]
+    MLdiag = np.array([-1,0,1])
+    ML = sp.sparse.spdiags(ML_Data, MLdiag, n, n).toarray()
+
+    # had to look this up, idk how it works to be honest need to figure that out
+    lower_MR = -lower
+    mid_MR = 2 - mid
+    upper_MR = -upper
+    
+    MR_data = [lower_MR, mid_MR, upper_MR]
+    MRdiag = np.array([-1,0,1])
+    MR = sp.sparse.diags(MR_data, MRdiag, n, n).toarray()
+    
+    MR[0, :] = 0
+    MR[0, 0] = 1
+    
+    return ML, MR
+
 
 
 def solveIceEquation(h, h_prime):
-    ML_Ice, MR_Ice = iceDensity() # fix this function
-    G = massBal(h, h_prime, timestep=v['dt']) # change timestep if needed idk if this is right
+    ML_Ice, MR_Ice = iceDensity(h, h_prime) # fix this function
+    G = massBal(h, h_prime, currentT) # change timestep if needed idk if this is right
     RHS = MR_Ice @ h + v['dt'] * G
     # need to apply boundary conditions 
+    RHS[0] = 0
     h_updated = np.linalg.inv(ML_Ice) @ RHS  # this math might be wrong, check later
     return h_updated
 
@@ -138,8 +196,12 @@ def solveIceEquation(h, h_prime):
 def solveBedrock(h, h_prime):
     ML_Bedrock, MR_Bedrock = bedrockM(v['n'])
     temp_var = h_prime - v['h_prime0'] + v['r'] * h # makes matrix math easier than it would be otherwise
+    temp_var[0] = 0
+    temp_var[-1] = 0
     temp_new = np.linalg.inv(ML_Bedrock) @ MR_Bedrock @ temp_var
     # do we need to apply boundary conditions here? maybe later
+    final_hPrime[0] = v['h_prime0'][0]
+    final_hPrime[-1] = v['h_prime0'][-1]
     final_hPrime = temp_new + v['h_prime0'] - v['r'] * h
     return final_hPrime
 
